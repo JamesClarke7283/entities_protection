1d6da9596180d3de1a1b8273a6b124d3684c9ab4
James David Clarke <james@jamesdavidclarke.com>
2024-01-05 02:19:59 +0000

    Added logging

--- init.lua
+++ init.lua
@@ -12,13 +12,18 @@ end
 -- Modify damage handling for entities
 local original_damage_function = mcl_damage.run_modifiers
 mcl_damage.run_modifiers = function(obj, damage, reason)
-    -- Check if the target is an entity and the damage source is a player
+    -- Check if the target is an entity (not a player) and if the source of damage is a player
     if obj and obj:get_luaentity() and not obj:is_player() and reason.source and reason.source:is_player() then
         local pos = obj:get_pos()
         local player_name = reason.source:get_player_name()
 
+        -- Debugging: Print a message to check if this part is being executed
+        minetest.log("action", "[areas_entities] Checking area protection for entity at " .. minetest.pos_to_string(pos))
+
         -- Check if the player is an owner of the area
         if not is_player_an_area_owner(player_name, pos) then
+            -- Debugging: Print a message when damage is prevented
+            minetest.log("action", "[areas_entities] Preventing damage by non-owner " .. player_name)
             return 0 -- Cancel damage if the player is not an area owner
         end
     end
