7edf980f0eefaeade64bb0c43ae11dd687645555
James David Clarke <james@jamesdavidclarke.com>
2024-01-07 02:09:23 +0000

    Fixes from NicoPro (Officially Bard)

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- Function to check if a player is an owner of the area
-local function is_player_an_area_owner(player_name, pos)
+local function is_player_an_area_owner(playername, pos)
     local owners = areas:getNodeOwners(pos)
     for _, owner in ipairs(owners) do
         if owner == player_name then
@@ -8,31 +8,30 @@ local function is_player_an_area_owner(player_name, pos)
     end
     return false
 end
+
 if minetest.get_modpath("mcl_damage") then
-minetest.log("error", "[areas_entities] mcl_damage detected, modifying damage handling for entities")
--- Modify damage handling for entities
-local original_damage_function = mcl_damage.run_modifiers
-mcl_damage.run_modifiers = function(obj, damage, reason)
-    if obj:get_luaentity() then
-    minetest.log("error", "[areas_entities] Damage function called for" .. obj:get_luaentity().name)
-    end
-    -- Check if the target is an entity (not a player) and if the source of damage is a player
-    if obj and not obj:is_player() and reason.source and reason.source:is_player() then
-        local pos = obj:get_pos()
-        local player_name = reason.source:get_player_name()
+    minetest.log("notice", "[areas_entities] mcl_damage detected, modifying damage handling for entities")
 
-        -- Debugging: Print a message to check if this part is being executed
-        minetest.log("action", "[areas_entities] Checking area protection for entity at " .. minetest.pos_to_string(pos))
+    -- Modify damage handling for entities
+    local original_damage_function = mcl_damage.run_modifiers
+    mcl_damage.run_modifiers = function(obj, damage, reason)
+        -- Check if the target is an entity (not a player) and if the source of damage is a player
+        if obj and not obj:is_player() and reason.source and reason.source:is_player() then
+            -- Get the position of the entity
+            local pos = obj:get_pos()
 
-        -- Check if the player is an owner of the area
-        if not is_player_an_area_owner(player_name, pos) then
-            -- Debugging: Print a message when damage is prevented
-            minetest.log("action", "[areas_entities] Preventing damage by non-owner " .. player_name)
-            return 0 -- Cancel damage if the player is not an area owner
+            -- Get the name of the player causing the damage
+            local player_name = reason.source:get_player_name()
+
+            -- Check if the player is an owner of the area
+            if not is_player_an_area_owner(player_name, pos) then
+                -- Prevent the damage if the player is not an owner
+                minetest.log("action", "[areas_entities] Preventing damage by non-owner " .. player_name)
+                return 0
+            end
         end
-    end
 
-    -- Call the original damage function for other cases
-    return original_damage_function(obj, damage, reason)
-end
+        -- Call the original damage function for other cases
+        return original_damage_function(obj, damage, reason)
+    end
 end
