10b47f841fe14832fc9166b512fb176b33282d69
James David Clarke <james@jamesdavidclarke.com>
2024-01-07 04:51:06 +0000

    Improve logging of source

--- init.lua
+++ init.lua
@@ -6,12 +6,26 @@ if minetest.get_modpath("mcl_damage") then
     local original_damage_function = mcl_damage.run_modifiers
     mcl_damage.run_modifiers = function(obj, damage, reason)
 
-      local reason_type = reason and reason.type or "<nil>"
-      local source_name = reason and reason.source and reason.source:get_player_name() or "<nil>"
-      local obj_name = obj and (obj:is_player() and obj:get_player_name() or (obj:get_luaentity() and obj:get_luaentity().name)) or "<nil>"
+      -- Log the reason type and the object name
+        local reason_type = reason and reason.type or "<nil>"
+        local obj_name = obj and (obj:is_player() and obj:get_player_name() or (obj:get_luaentity() and obj:get_luaentity().name)) or "<nil>"
 
-      minetest.log("action", "[areas_entities] Damage Modifier Called - Reason Type: " .. reason_type ..
-                   ", Source Name: " .. source_name .. ", Object Name: " .. obj_name)
+        -- Determine and log the source name
+        local source_name = "<nil>"
+        if reason and reason.source then
+            if reason.source:is_player() then
+                source_name = reason.source:get_player_name()
+            elseif reason.source.get_luaentity then
+                local lua_entity = reason.source:get_luaentity()
+                source_name = lua_entity and lua_entity.name or "<non-player entity>"
+            else
+                source_name = "<unknown source>"
+            end
+        end
+
+        minetest.log("action", "[areas_entities] Damage Modifier Called - Reason Type: " .. reason_type ..
+                    ", Source Name: " .. source_name .. ", Object Name: " .. obj_name)
+
 
         -- Check if the target is an entity (not a player) and if the source of damage is a player
         if obj and not obj:is_player() and reason.source and reason.source:is_player() then
