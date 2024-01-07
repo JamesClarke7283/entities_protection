19c33c2b39a39d9c907ebf867f12a5d2465a941f
James David Clarke <james@jamesdavidclarke.com>
2024-01-07 03:21:06 +0000

    More fixes

--- init.lua
+++ init.lua
@@ -46,13 +46,17 @@ for _, entity in pairs(minetest.registered_entities) do
     update_entity_on_punch(entity)
 end
 
--- Function called when all mods have finished loading
 minetest.register_on_mods_loaded(function()
     minetest.log("action", "[areas_entities] Server restarted, resetting entity punch overrides.")
-    for _, obj in ipairs(minetest.get_objects_inside_radius({x=0, y=0, z=0}, 50000)) do
-        local lua_entity = obj:get_luaentity()
-        if lua_entity then
-            lua_entity._areas_entities_updated = false
+    local objects = minetest.get_objects_inside_radius({x=0, y=0, z=0}, 50000)
+    if objects then
+        for _, obj in ipairs(objects) do
+            local lua_entity = obj:get_luaentity()
+            if lua_entity then
+                lua_entity._areas_entities_updated = false
+            end
         end
+    else
+        minetest.log("error", "[areas_entities] Unable to retrieve objects for reset.")
     end
 end)
