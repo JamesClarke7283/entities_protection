5c5b3170437535b4b4b935977e85ce880d4f30cc
James David Clarke <james@jamesdavidclarke.com>
2024-01-07 02:49:07 +0000

    Hopefully fixes it

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 -- Function to check if a player is an owner of the area
 local function is_player_an_area_owner(player_name, pos)
     local owners = areas:getNodeOwners(pos)
-    minetest.log("action", "[areas_entities] Ran is_player_an_area_owner with owners: "..minetest.serialize(owners))
     for _, owner in ipairs(owners) do
         if owner == player_name then
             return true
@@ -10,32 +9,48 @@ local function is_player_an_area_owner(player_name, pos)
     return false
 end
 
--- Override the entity punch function
-local function override_entity_punch()
-    -- Iterate over all registered entities
-    for _, entity in pairs(minetest.registered_entities) do
-        -- Store the original on_punch function
-        minetest.log("action", "[areas_entities] On Punch: "..minetest.serialize(entity.on_punch))
-        local original_on_punch = entity.on_punch
+-- Globalstep function to monitor entity punches
+minetest.register_globalstep(function(dtime)
+    for _, obj in ipairs(minetest.get_objects_inside_radius({x=0, y=0, z=0}, 50000)) do
+        if obj:is_player() == false then -- Exclude players
+            local lua_entity = obj:get_luaentity()
+            if lua_entity and lua_entity.punched then
+                -- The entity has been punched, reset the flag
+                lua_entity.punched = nil
 
-        -- Override the on_punch function
-        entity.on_punch = function(self, hitter, time_from_last_punch, tool_capabilities, dir, damage)
-            -- Check if the hitter is a player and the entity is not a player
-            if hitter and hitter:is_player() and not self.object:is_player() then
-                local pos = self.object:get_pos()
-                local player_name = hitter:get_player_name()
+                -- Check if the puncher is a player
+                local puncher = lua_entity.last_puncher
+                if puncher and puncher:is_player() then
+                    local player_name = puncher:get_player_name()
+                    local pos = obj:get_pos()
 
-                -- Check if the player is an owner of the area
-                if not is_player_an_area_owner(player_name, pos) then
-                    -- Prevent the damage if the player is not an owner
-                    damage = 0
-                    minetest.log("action", "[areas_entities] Preventing damage by non-owner " .. player_name)
-                    return true
+                    -- Check if the player is an owner of the area
+                    if not is_player_an_area_owner(player_name, pos) then
+                        -- Prevent further processing
+                        minetest.log("action", "[areas_entities] Preventing damage by non-owner " .. player_name)
+                        lua_entity.hp = lua_entity.old_hp -- Reset HP to previous value
+                    end
                 end
             end
+        end
+    end
+end)
 
-            -- Otherwise, call the original on_punch function
-            if original_on_punch then
+-- Override the entity punch function
+local function override_entity_punch()
+    for _, entity in pairs(minetest.registered_entities) do
+        if entity.on_punch then
+            -- Store the original on_punch function
+            local original_on_punch = entity.on_punch
+
+            -- Override the on_punch function
+            entity.on_punch = function(self, hitter, time_from_last_punch, tool_capabilities, dir, damage)
+                -- Mark the entity as punched and store the puncher and HP
+                self.punched = true
+                self.last_puncher = hitter
+                self.old_hp = self.hp or 0
+
+                -- Call the original on_punch function
                 original_on_punch(self, hitter, time_from_last_punch, tool_capabilities, dir, damage)
             end
         end
